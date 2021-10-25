441299f6d852d79bd2a943f5027fed7eee9ed06c
cecyrang <cecilia21c@naver.com>
2021-10-25 17:46:09 +0900

    MemberVO - id , name 등록

--- pkr-webproject/src/main/java/model/MemberVO.java
+++ pkr-webproject/src/main/java/model/MemberVO.java
@@ -1,5 +1,29 @@
 package model;
 
 public class MemberVO {
+	private String name ; 
+	private String id ;
+	public MemberVO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public MemberVO(String name, String id) {
+		super();
+		this.name = name;
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	
 	
 }
